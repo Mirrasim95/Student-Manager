de3c257265b5a68dc21b977731ed845578f40411
Rasim4ik111 <rasim95suleymanli@gmail.com>
2026-04-12 20:48:04 +0400

    added options

---
+++ student-manager.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main( ) {
+    int pick;
+
+    while(1) {
+        printf("add user\n");
+        printf("delete user\n");
+        printf("show user\n");
+        printf("uptdate user\n");
+        printf("exit\n");
+        
+
+        printf("write option: ");
+
+    }
+}
